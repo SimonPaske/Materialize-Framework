cffd76936c6d7938110b30e7502a50ed53637c0f
SimonPaske <>
2023-08-02 11:07:49 +0000

    coverted back previous code __init__.py

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -5,22 +5,15 @@ from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
     import env  # noqa
 
-db = SQLAlchemy()
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
-def create_app():
-    app = Flask(__name__)
-    app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
-    if os.environ.get("DEVELOPMENT") == "True":
-        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
-    else:
-        uri = os.environ.get("DATABASE_URL")
-        if uri.startswith("postgres://"):
-            uri = uri.replace("postgres://", "postgresql://", 1)
-        app.config["SQLALCHEMY_DATABASE_URI"] = uri
-
-    from taskmanager import routes  # noqa
-    db.init_app(app)
-    return app
-
-
+from taskmanager import routes  # noqa
