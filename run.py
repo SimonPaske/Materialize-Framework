aaeafc208043063990237e1d036bb00536e9c55b
SimonPaske <>
2023-08-03 10:15:22 +0000

    Add function to run.py

--- run.py
+++ run.py
@@ -2,6 +2,13 @@ import os
 from taskmanager import app
 
 
+def create_tables(self):
+    with app.app_context():
+        db.create_all()
+
+
+create_tables(db)
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP"),
